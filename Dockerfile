c72c609dcc8a47712ce426fd644bcfe1766a4b80
Ashish Sapra <a.ash.bus@gmail.com>
2021-08-12 15:04:54 +0530

    re-commit

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM continuumio/anaconda3
-COPY . /usr/app/
-EXPOSE 5000
-WORKDIR /usr/app/
-RUN pip install -r requirements.txt
-CMD streamlit run app.py
-
--- app.py
+++ app.py
@@ -1,58 +1,99 @@
-import streamlit as st
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  6 02:17:30 2021
+
+@author: ashishsapra
+"""
+
+# from flask import Flask, render_template, request
+import jsonify
+import requests
 import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+import streamlit as st
+#app = Flask(__name__)
+model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+@app.route('/',methods=['GET'])
+'''def Home():
+    return render_template('index.html')
 
-model = pickle.load(open('random_forest_regression_model.pkl','rb'))
 
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])'''
+
+
+def predict():
+    
+     """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: Year
+        in: query
+        type: number
+        required: true
+      - name: Showroom_Price
+        in: query
+        type: number
+        required: true
+      - name: KMs_Driven
+        in: query
+        type: number
+        required: true
+      - name: owner
+        in: query
+        type: number
+        required: true
+    - name: owner
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    
+    
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Present_Price=float(request.form['Present_Price'])
+        Kms_Driven=int(request.form['Kms_Driven'])
+        Kms_Driven2=np.log(Kms_Driven)
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol=='Petrol'):
+                Fuel_Type_Petrol=1
+                Fuel_Type_Diesel=0
+        else:
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=1
+        Year=2020-Year
+        Seller_Type_Individual=request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual=1
+        else:
+            Seller_Type_Individual=0	
+        Transmission_Mannual=request.form['Transmission_Mannual']
+        if(Transmission_Mannual=='Mannual'):
+            Transmission_Mannual=1
+        else:
+            Transmission_Mannual=0
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+    else:
+        return render_template('index.html')
+    
 def main():
-    st.title("Car Selling Price Prediction")
-    st.markdown("#### Do you want to sell your Car? \n#### Check your selling price.......")
+    
 
-    st.write('')
-    st.write('')
-
-    years = st.number_input("Which year you have purchased car?",1990, 2021, step=1, key='year')
-    Years_old = 2021-years
-
-    Present_Price = st.number_input('What is on-road price of the car? (In Lakhs)', 0.00, 50.00, step=0.5, key="present_price")
-
-    Kms_Driven = st.number_input('How much distance covered till now?',0.00, 500000.00, step=500.00, key = 'drived')
-
-    Owner = st.radio("The number of owners car hd previously?", (0, 1, 3), key='owner')
-
-    Fuel_Type_Petrol = st.selectbox('What is fuel type of the car?', ('Petrol', 'Diesel', 'CNG'), key = 'fuel')
-    if(Fuel_Type_Petrol== 'Petrol'):
-        Fuel_Type_Petrol = 1
-        Fuel_Type_Diesel = 0
-    elif(Fuel_Type_Petrol == 'Diesel'):
-        Fuel_Type_Petrol = 0
-        Fuel_Type_Diesel = 1
-    else:
-        Fuel_Type_Petrol = 0
-        Fuel_Type_Diesel = 0
-
-    Seller_Type_Individual = st.selectbox('Are you a dealer or an individual?', ('Dealer', 'Individual'), key='dealer')
-    if Seller_Type_Individual == 'Individual':
-        Seller_Type_Individual = 1
-    else:
-        Seller_Type_Individual = 0
-    Transmission_Mannual = st.selectbox('What is Transmission Type?', ('Manual', 'Automatic'), key='manual')
-    if Transmission_Mannual == 'Mannual':
-        Transmission_Mannual = 1
-    else:
-        Transmission_Mannual = 0
-
-    if st.button("Predict Price", key='predict'):
-        try:
-            Model = model
-            predicition = Model.predict([[Present_Price, Kms_Driven, Owner, Years_old, Fuel_Type_Diesel, Fuel_Type_Petrol, Seller_Type_Individual, Transmission_Mannual]])
-            output = round(predicition[0],2)
-            if output < 0:
-                st.warning("You can't sell car!")
-            else:
-                st.success("You can sell car for {}".format(output))
-
-        except:
-            st.warning("Something went wrong\n Try Again")
-
-if __name__ == '__main__':
-    main()
+if __name__=="__main__":
+    app.run(debug=True)
